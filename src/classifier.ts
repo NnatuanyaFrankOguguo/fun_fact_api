446d10276bd34dbf992cebb5c29309ee9acb2d75
NnatuanyaFrankOguguo <nnatuanyafrank@gmail.com>
2025-02-05 14:56:39 +0100

    ok

--- src/classifier.ts
+++ src/classifier.ts
@@ -1,25 +1,13 @@
-import {isPrime, isArmstrong, isPerfectSqr, getFunFact} from './utils'
+// import {isPrime, isArmstrong, isPerfectSqr, getFunFact} from './utils'
 
 
-export const classifyNumber = async (num: number): Promise<{number: number,
-    is_prime: boolean,
-    is_perfect: boolean,
-    properties: string[],
-    digit_sum: number,
-    fun_fact: string} > => {
-    const properties: string[] = [];
+// export const classifyNumber = async (num: number): Promise<{number: number,
+//     is_prime: boolean,
+//     is_perfect: boolean,
+//     properties: string[],
+//     digit_sum: number,
+//     fun_fact: string} > => {
     
-    if(isArmstrong(num)) properties.push("armstrong");
-    properties.push(num % 2 === 0 ? "even" : "odd");
-
-    return {
-        number: num,
-        is_prime: isPrime(num),
-        is_perfect: isPerfectSqr(num),
-        properties,
-        digit_sum: num.toString().split("").reduce((sum, digit) => sum + parseInt(digit), 0),
-        fun_fact: await getFunFact(num),
-    };
 
     
-};
+// };
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from 'dotenv';
-import {classifyNumber} from './classifier';
 import compression from 'compression';
+import {isPrime, isArmstrong, isPerfectSqr, getFunFact} from './utils'
 
 
 dotenv.config();
@@ -24,9 +24,19 @@ app.get('/api/classify-number', async (req: Request, res: Response) => {
                 error: true,
             });
         }
-        const result = await classifyNumber(Number(num));
-        res.json(result); // Send response properly
+        const properties: string[] = [];
+    
+        if(isArmstrong(Number(num))) properties.push("armstrong");
+        properties.push(Number(num) % 2 === 0 ? "even" : "odd");
 
+        res.status(200).json({
+            number: num,
+            is_prime: isPrime(Number(num)),
+            is_perfect: isPerfectSqr(Number(num)),
+            properties,
+            digit_sum: Number(num).toString().split("").reduce((sum, digit) => sum + parseInt(digit), 0),
+            fun_fact: await getFunFact(Number(num)),
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internal server error" });
